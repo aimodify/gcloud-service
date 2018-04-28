445f52f7af14d28f38e1955afcd0fe9add71cbee
Eugeny Chaban <277617@gmail.com>
2018-04-28 10:38:41 +0300

    composer, base class

---
+++ src/Aimodify/GColudService/GCloudService.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ai
+ * Date: 28.04.18
+ * Time: 10:27
+ */
+
+namespace Aimodify\GColudService;
+
+class GCloudService
+{
+    protected $name;
+
+    protected $driver;
+
+    protected $credentials;
+
+    public function __construct()
+    {
+
+    }
+}
